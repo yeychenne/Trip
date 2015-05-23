a089ae8ef21a040f452490bb4c75e6c70c1bdb6c
red-gns <maha.elbayad@gmail.com>
2015-05-23 10:47:18 +0200

    delete

--- config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'local' : "mongodb://localhost:27017/dbTrip",
-    'bluemix' : "mongodb://IbmCloud_o5j828v7_mk9qruge_1k0decef:_OnhvdBFD5eT6FXou6eCvQcNecfzDuKS@ds055200.mongolab.com:55200/IbmCloud_o5j828v7_mk9qruge"
-
-};
